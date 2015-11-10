20393f5a3e8ab7cbfaee3f26b9e2d7c3db00e8a3
Lucius Guthrie <lcguthrie@crimson.ua.edu>
2015-11-09 22:20:03 -0600

    login and add property draft

---
+++ app/src/main/java/com/atease/at_ease/AddPropertyActivity.java
@@ -0,0 +1,7 @@
+package com.atease.at_ease;
+
+/**
+ * Created by lguthrie825 on 11/9/15.
+ */
+public class AddPropertyActivity {
+}
---
+++ app/src/main/java/com/atease/at_ease/SignUpActivity.java
@@ -0,0 +1,7 @@
+package com.atease.at_ease;
+
+/**
+ * Created by lguthrie825 on 11/5/15.
+ */
+public class SignUpActivity {
+}
